e90a6854c0e5bd22cea50578edd7cd453b136daf
Ahmet YILDIRIM <ahmetyildirim@live.com>
2018-09-02 23:52:46 +0300

    "props" kullanımı ile komponentlerde veri iletişimi.

--- vue-cli-intro/src/App.vue
+++ vue-cli-intro/src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <div class="container">
-      <app-header></app-header>
-    <hr>
-    <div class="row">
-      <app-servers></app-servers>
-      <app-servers-details></app-servers-details>
-    </div>
-    <hr>
-    <app-footer></app-footer>
-  </div>
+  <app-user></app-user>
 </template>
 
 <script>
-import header from "./components/Common/Header";
-import footer from "./components/Common/Footer";
-import server from "./components/Server/Server";
-import serverdetails from "./components/Server/ServerDetails";
+import User from "./components/User";
+
 export default{
   components : {
-    appHeader : header,
-    appFooter : footer,
-    appServers : server,
-    appServersDetails : serverdetails,
+    appUser : User
   }
 }
 </script>
---
+++ vue-cli-intro/src/components/UserDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-md-6">
+    <h3>Child Component 1</h3>
+    <p>Ben User.vue isimli Parent Component'in içerisindeki bir Child componentim</p>
+    <p>Kullanıcı Adı : {{ name }} </p>
+    <p>Kullanıcı Adı : {{ switchName() }} </p>
+  </div>
+</template>
+<script>
+ export default{
+   props : ["name"],
+   methods : {
+     switchName(){
+       // split("") name değerini boşluklardan bölerek diziye aktarır,
+       // reverse() diziyi tersten sıralar.
+       // join("") aralarına boşluk koyarak birleştirir, yani name tersten yazdırılmış olur.
+       return this.name.split("").reverse().join("");
+     }
+   }
+ }
+</script>
+<style scoped>
+  div {
+    background-color: lightcoral;
+    padding: 20px;
+    border: 1px solid #666;
+    display: inline-block;
+  }
+</style>
